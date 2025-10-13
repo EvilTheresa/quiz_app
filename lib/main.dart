e1f4a9c76e99dbbbb004602400f4d67f1d32b2cd
EvilTheresa <ruslan.null@gmail.com>
2025-10-13 11:00:06 +0600

    First screen almost ready

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:quiz_project/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+          ),
+          child: const StartScreen())),
+    ),
+  );
 }
