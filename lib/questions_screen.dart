b5a0129a77f382e70c3cc4e6cadfcd7150798d29
EvilTheresa <ruslan.null@gmail.com>
2025-10-15 13:37:53 +0600

    Styling and optimizing question screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_project/answer_button.dart';
-import 'package:quiz_project/start_screen.dart';
+import 'package:quiz_project/models/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -14,17 +14,30 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreen extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('The question'),
-          SizedBox(height: 30),
-          AnswerButton(answerText: 'Answer text', onTap: () {}),
-          AnswerButton(answerText: 'Answer text', onTap: () {}),
-          AnswerButton(answerText: 'Answer text', onTap: () {}),
-        ],
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+
+          children: [
+            Text(
+                currentQuestion.text,
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            })
+          ],
+        ),
       ),
     );
   }
